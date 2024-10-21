5f85c11cbe353cf29e6201283bb6c2043ffb7c99
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2024-10-21 10:39:13 -0400

    init

--- frontend/server.js
+++ frontend/server.js
@@ -1,16 +1,16 @@
 //simple express server to run frontend production build;
-var https = require('https');
+const https = require('https');
 const express = require("express");
 const path = require("path");
 const app = express();
-var fs = require('fs');
-var privateKey  = fs.readFileSync('/etc/nginx/ssl/zap.conectapolitico.com.br/2394779/server.key', 'utf8');
-var certificate = fs.readFileSync('/etc/nginx/ssl/zap.conectapolitico.com.br/2394779/server.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+const fs = require('fs');
+const privateKey  = fs.readFileSync('/etc/nginx/ssl/zap.conectapolitico.com.br/2394779/server.key', 'utf8');
+const certificate = fs.readFileSync('/etc/nginx/ssl/zap.conectapolitico.com.br/2394779/server.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-var httpsServer = https.createServer(credentials, app)
+const httpsServer = https.createServer(credentials, app)
 httpsServer.listen(443, 'zap.conectapolitico.com.br');
 
